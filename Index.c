b80f39c7bf201beebcd2a7fcbe5d5b86c15db959
Mauriciodeoliveira007 <mauriciodeoliveira06@gmail.com>
2025-08-30 18:55:05 -0300

    Criar struct e funções de leitura

--- Index.c
+++ Index.c
@@ -4,9 +4,46 @@
 #include <ctype.h>
 #include <math.h>
 #include <time.h>
+//STRUCT=============================================================================================================
+typedef struct c
+{
+    char nome[255];
+    float peso;
+    int calorias;
+} Dieta;
+//PROTOTIPAÇÃO========================================================================================================
+
+//====================================================================================================================
 
 int main()
 {
     
     return 0;
+}
+
+//=====================================================================================================================
+// - Crie uma função que obtém os dados de uma estrutura dieta pelo teclado e a retorna.
+
+//Funções de Input=====================================================================================================
+int lerInteiro() {
+    char str[255] = 0;
+    int transf = 0;
+    fgets(str, sizeof(str), stdin);
+    sscanf(str, "%d", &transf);
+
+    return transf;
+}
+
+float lerFloat() {
+    char str[255] = 0;
+    int transf = 0;
+    fgets(str, sizeof(str), stdin);
+    sscanf(str, "%d", &transf);
+
+    return transf;
+}
+
+void lerString(char palavra[], int tam) {
+    fgets(palavra, tam, stdin);
+    palavra[strlen(palavra) - 1] = '\0';
 }
